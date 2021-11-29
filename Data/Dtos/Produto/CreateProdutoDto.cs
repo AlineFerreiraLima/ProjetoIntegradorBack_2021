c7d27801ec4063f39a3be65014a5f7bd84af4897
Aline Ferreira de Lima <69773709+AlineFerreiraLima@users.noreply.github.com>
2021-11-29 18:44:53 -0300

    Add files via upload

---
+++ Data/Dtos/Produto/CreateProdutoDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AtacadoAPI.Data.Dtos.Produto
+{
+    public class CreateProdutoDto
+    {
+        [Required(ErrorMessage = "Nome do Produto é obrigatório mínimo 3, máximo 30 caracteres")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Valor do Produto é obrigatório")]
+        public Double Valor { get; set; }
+        [Required(ErrorMessage = "Campo Fornecedor ID é obrigatório!")]
+        public int FornecedorId { get; set; }
+        [Required(ErrorMessage = "Status do Produto é obrigatório")]
+        public Boolean Status { get; set; }
+        [Required(ErrorMessage = "Quantidade do Produto é obrigatório")]
+        public Double Quantidade { get; set; }
+    }
+}
